38b080d1ec48253147ef835d4f3a228ce0a8a3dc
ELIZA <rysaeva96@mail.ru>
2022-05-21 14:40:59 +0500

    Решение 23 задачи

--- Task_4/Program4.cs
+++ Task_4/Program4.cs
@@ -1,1 +1,9 @@
-﻿
+﻿// Напишите программу, которая принимает на
+// вход число (N) и выдаёт таблицу кубов 
+// чисел от 1 до N.
+
+Console.WriteLine("Введите число N");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("|{0,5}|{1,9}|", "Число", "Куб числа");
+for (int i = 1; i <= length; i++) 
+Console.WriteLine("|{0,5}|{1,9}|", i, i*i*i);   
